69c6c631c2af97c1b2fcdd9e9e072dc48713540e
urouro0615 <40481946+urouro0615@users.noreply.github.com>
2018-08-30 13:38:14 +0900

    Fragment committed.

--- app/src/main/java/com/dokkyo/horieseminar/dokkyo/MainActivity.java
+++ app/src/main/java/com/dokkyo/horieseminar/dokkyo/MainActivity.java
@@ -3,45 +3,62 @@ package com.dokkyo.horieseminar.dokkyo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_schedule:
-                    mTextMessage.setText(R.string.title_schedule);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-                case R.id.navigation_unit:
-                    mTextMessage.setText(R.string.title_unit);
-                    return true;
-                case R.id.navigation_mail:
-                    mTextMessage.setText(R.string.title_mail);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new ScheduleFragment());
     }
 
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch(item.getItemId()){
+
+            case R.id.navigation_schedule:
+                fragment = new ScheduleFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new NotificationsFragment();
+                break;
+
+            case R.id.navigation_unit:
+                fragment = new UnitFragment();
+                break;
+
+            case R.id.navigation_mail:
+                fragment = new MailFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
 }
